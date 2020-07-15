97b67f9cb8c10d75f714cf6d69d08e53ba6d28ee
krisDziedzic <kris.dziedzic@neos.co.uk>
2020-07-15 18:16:57 +0200

    Add home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _currentBottomNavigationIndex = 0;
+  final List<Widget> _bottomNavigationPages = [
+    Center(child: Text('Page1'),),
+    Center(child: Text('Page2'),),
+  ];
+
+  void _onBottomNavigationTapped(int index) {
+    setState(() {
+      _currentBottomNavigationIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _bottomNavigationPages[_currentBottomNavigationIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => _onBottomNavigationTapped(index),
+        currentIndex: _currentBottomNavigationIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.work),
+            title: Text('Tasks'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note),
+            title: Text('Notes')
+          )
+        ],
+      ),
+    );
+  }
+}
