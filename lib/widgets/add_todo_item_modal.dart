0a06fc287eda6f50ee66492446b854456ad81bc0
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-07-17 20:24:55 +0200

    Add new todo card styling

--- lib/widgets/add_todo_item_modal.dart
+++ lib/widgets/add_todo_item_modal.dart
@@ -21,14 +21,28 @@ class _AddTodoItemModalState extends State<AddTodoItemModal> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(vertical: 48.0, horizontal: 32.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text("Add your todo stuff"),
+          Text("Add a new todo task",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 22,
+          ),
+          ),
+          SizedBox(height: 20,),
           TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter a title',
+            ),
             controller: _todoNameController,
           ),
+          SizedBox(height: 20,),
           FlatButton(
+            color: Theme.of(context).primaryColor,
+            textColor: Theme.of(context).primaryTextTheme.headline1.color,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
             child: Text("Add item"),
             onPressed: _addTodoItem,
           )
