d71f2ea6aad96c890777ab2e2304bfbf4f10d0b6
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-07-18 16:50:37 +0200

    Make todo item dismissible

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -6,29 +6,61 @@ class TodoItem extends StatelessWidget {
 
   TodoItem({this.todoId, this.todoTitle});
 
+  Future<bool> showDeleteConfirmationDialog(BuildContext buildContext) async {
+    return await showDialog(
+        context: buildContext,
+        builder: (dialogContext) {
+          return AlertDialog(
+            title: const Text("Confirm"),
+            content: const Text("Are you sure you wish to delete this item?"),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('Yes'),
+                textColor: Colors.red,
+                onPressed: () => Navigator.of(buildContext).pop(true),
+              ),
+              FlatButton(
+                child: const Text('No'),
+                onPressed: () => Navigator.of(buildContext).pop(false),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-          Text(todoTitle),
-          Row(
-          children: <Widget>[
-          IconButton(icon: Icon(Icons.delete),
-          onPressed: () {
-
-           },),
-          IconButton(icon: Icon(Icons.edit), 
-          onPressed: () {
-
-          },),
-          ],),
-        ],),
-      )
-    ,);
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        padding: EdgeInsets.only(right: 16.0),
+        alignment: Alignment.centerRight,
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+      confirmDismiss: (dismissDirection) async {
+        return await showDeleteConfirmationDialog(context);
+      },
+      onDismissed: (direction) {},
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(todoTitle),
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
